86048083c55334c68c8c5a24c6c5eef2f166b068
injulme <injulme0309@naver.com>
2023-11-08 23:32:26 +0900

    feat: scroll-animation 진행중

---
+++ 4-scroll-animation/src/main.js
@@ -0,0 +1,76 @@
+import * as THREE from "three";
+
+window.addEventListener("load", () => {
+  init();
+});
+
+function init() {
+  const canvas = document.querySelector("#canvas");
+
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true,
+    canvas,
+  });
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const scene = new THREE.Scene();
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    500
+  );
+
+  camera.position.set(0, 25, 150);
+
+  const waveGeometry = new THREE.PlaneGeometry(1500, 1500, 150, 150);
+  const waveMaterial = new THREE.MeshStandardMaterial({
+    // wireframe: true,
+    color: "#00ffff",
+  });
+
+  const wave = new THREE.Mesh(waveGeometry, waveMaterial);
+
+  wave.rotation.x = -Math.PI / 2;
+
+  const waveHeight = 2.5;
+
+  for (let i = 0; i < waveGeometry.attributes.position.count; i++) {
+    const z =
+      waveGeometry.attributes.position.getZ(i) +
+      (Math.random() - 0.5) * waveHeight;
+
+    waveGeometry.attributes.position.setZ(i, z);
+  }
+
+  scene.add(wave);
+
+  const pointLight = new THREE.PointLight(0xffffff, 1);
+
+  pointLight.position.set(15, 15, 15);
+
+  scene.add(pointLight);
+
+  render();
+
+  function render() {
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener("resize", handleResize);
+}
